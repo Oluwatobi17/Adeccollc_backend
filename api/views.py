5f6d4b2a3c7a61e65f791d79fe65a4aac139ec5f
Oluwatobi17 <36605687+Oluwatobi17@users.noreply.github.com>
2025-02-09 10:21:16 +0100

    Update views.py

--- api/views.py
+++ api/views.py
@@ -16,7 +16,7 @@ class SendFormEmail(View):
         # print(data);
         subject = 'New Application'
         message = data;
-        recipient_list = ['ganiuibrahim3000@gmail.com']  # Replace with the recipient's email addresses
+        recipient_list = ['hardycre.co@gmail.com']  # Replace with the recipient's email addresses
 
         send_custom_email(subject, message, recipient_list)
 

375a73769485c8b60c916106921bfb97d65c23d7
Oluwatobi17 <36605687+Oluwatobi17@users.noreply.github.com>
2025-07-01 06:15:40 +0100

    Update views.py

--- api/views.py
+++ api/views.py
@@ -46,6 +46,30 @@ class SendFormEmailAirMax(View):
 
         return HttpResponse("Email Send Successfully!");
 
+class SendFormEmailAirJordan(View):
+    def get(self, request):
+        first_name = request.GET.get('first_name', None)
+        last_name = request.GET.get('last_name', None)
+        zipcode = request.GET.get('zipcode', None)
+        email = request.GET.get('email', None)
+        address = request.GET.get('address', None)
+        phone = request.GET.get('phone', None)
+        age = request.GET.get('age', None)
+        nearest_nike = request.GET.get('nearest_nike', None)
+        nearest_walmart = request.GET.get('nearest_walmart', None)
+
+        data = " First Name: "+first_name+"\n Last Name: "+last_name+"\n Zipcode: "+zipcode+"\n Email: "+email+"\n Address: "+address+"\n Phone: "+phone+"\n Nearest Nike Store: "+nearest_nike+"\n Nearest Waltmart Store: "+nearest_walmart+"\n Age: "+age;
+        # """.format(first_name,last_name,email,address,phone);
+
+        # print(data);
+        subject = 'New Application'
+        message = data;
+        recipient_list = ['CHANGE_TO_GIVEN_MAIL']  # Replace with the recipient's email addresses
+
+        send_custom_email(subject, message, recipient_list)
+
+        return HttpResponse("Email Send Successfully!");
+
 # def my_email_view(request):
 #     # ... Your view logic ...
 #     print("Job application received ", request.POST['email']);

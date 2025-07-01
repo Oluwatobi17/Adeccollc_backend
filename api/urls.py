9a4a6ce827415eaa72418d2cba8f24e5b3c30ba5
Oluwatobi17 <36605687+Oluwatobi17@users.noreply.github.com>
2025-07-01 06:15:19 +0100

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('apply/job/', views.SendFormEmail.as_view(), name="apply"),
     path('airmax/apply/job/', views.SendFormEmailAirMax.as_view(), name="Airmax_apply"),
+    path('airjordan/apply/job/', views.SendFormEmailAirJordan.as_view(), name="Airjordan_apply"),
 ]

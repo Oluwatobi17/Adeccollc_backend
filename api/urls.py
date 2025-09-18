643e1e5c20195f745bb18300ed4479fcc8e52bef
Oluwatobi17 <36605687+Oluwatobi17@users.noreply.github.com>
2025-09-18 12:59:16 +0100

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,5 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from . import views
 
 urlpatterns = [
-    path('apply/job/', views.SendFormEmail.as_view(), name="apply"),
-    path('airmax/apply/job/', views.SendFormEmailAirMax.as_view(), name="Airmax_apply"),
     path('airjordan/apply/job/', views.SendFormEmailAirJordan.as_view(), name="Airjordan_apply"),
 ]
